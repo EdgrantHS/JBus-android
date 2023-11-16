5ef97e6b1b28810de24b02668a9e0dd652d20c71
EdgrantHS <edgrant.h@gmail.com>
2023-11-16 15:43:06 +0700

    TP08 - experiment

--- app/src/main/java/com/edgrantJBusRD/jbus_android/LoginActivity.java
+++ app/src/main/java/com/edgrantJBusRD/jbus_android/LoginActivity.java
@@ -2,7 +2,13 @@ package com.edgrantJBusRD.jbus_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText emailEditText = (EditText) findViewById(R.id.loginEmail);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailEditText.getText().toString();
+
+                if (!isValidEmail(email)) {
+                    Toast.makeText(LoginActivity.this, "Invalid email format. Please try again.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private boolean isValidEmail(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 }
