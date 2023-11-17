c4be833f57b5720e0840a7b71b71750fb99c980f
EdgrantHS <edgrant.h@gmail.com>
2023-11-17 11:53:35 +0700

    CS08 - Hide banner main activity

--- app/src/main/java/com/edgrantJBusRD/jbus_android/RegisterActivity.java
+++ app/src/main/java/com/edgrantJBusRD/jbus_android/RegisterActivity.java
@@ -4,10 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //hide action bar dengan null error exeption
+        try {
+            Objects.requireNonNull(getSupportActionBar()).hide();
+        }
+        catch (NullPointerException nullPointerException){
+            System.out.println("nullPointerExeption");
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
